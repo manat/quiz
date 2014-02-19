66b6c41d36c243826572308f8d172c10b253969a
Manat Lertpeetipat <mlertpee@gmail.com>
2014-02-20 04:07:01 +0700

    Narrow tab override to only TextArea with editor class.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,6 @@ jQuery(function ($) {
     'use strict';
 
     // enable Tab Override for all textareas
-    $('textarea').tabOverride();
+    $('textarea.editor').tabOverride();
     $.fn.tabOverride.tabSize(4).autoIndent(false);
 });
